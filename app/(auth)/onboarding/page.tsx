7db3348558be05bf9fee88b01943b587fa87e85a
prabhanjan sharma <90155386+prab002@users.noreply.github.com>
2024-10-17 17:51:12 +0530

    done with auth functionality with clerk

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,9 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+async function page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>Hello World ❂</h1>
+      <h1 className="text-2xl font-bold text-whites">Onboarding</h1>
+      <UserButton />
     </main>
   );
 }
+
+export default page;

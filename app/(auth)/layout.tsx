50f699a946d8b24785c4a8f0d5360e2e26bf2f45
prabhanjan sharma <90155386+prab002@users.noreply.github.com>
2024-10-17 17:49:55 +0530

    added the login functionality with clerk'

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "Thread App",
+  description: "A Next.js app which is a clone of Thread",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="flex items-center justify-center h-screen">
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}

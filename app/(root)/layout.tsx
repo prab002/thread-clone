8446e81a88b8ead54128f7f8f9c70b6665d40d37
prabhanjan sharma <90155386+prab002@users.noreply.github.com>
2024-10-18 11:15:15 +0530

    done with navbar form home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,12 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "../globals.css";
 
+import Topbar from "@/components/shared/Topbar";
+import Leftbar from "@/components/shared/Leftbar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import { ClerkProvider } from "@clerk/nextjs";
+
 const geistSans = localFont({
   src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -27,12 +33,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <Topbar />
+          <main>
+            <Leftbar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }

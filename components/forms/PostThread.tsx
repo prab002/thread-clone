250e6b8eacb18d16a7e37cd6f7eba8c0522ec1c4
prabhanjan sharma <90155386+prab002@users.noreply.github.com>
2024-10-20 10:21:22 +0530

    create thread is workign

---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { useRouter, usePathname } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+import { ThreadValidation } from "@/lib/validation/threadValidation";
+import { createThread } from "@/lib/actions/thread.action";
+
+interface PostThreadProps {
+  userId: string;
+}
+
+function PostThread({ userId }: PostThreadProps) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm<z.infer<typeof ThreadValidation>>({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+      thread: "",
+      accountId: userId,
+    },
+  });
+
+  // Handle form submission
+  const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+    await createThread({
+      text: values.thread,
+      author: userId,
+      communityId: null,
+      path: pathname,
+    });
+
+    router.push("/");
+  };
+
+  return (
+    <Form {...form}>
+      <form 
+        className="flex flex-col justify-start gap-10"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col gap-3">
+              <FormLabel className="text-base-semibold text-light-2">
+                Content
+              </FormLabel>
+              <FormControl className="no-focus border border-dark-2 bg-dark-3 text-light-1">
+                <Textarea rows={15} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="self-end">
+          Submit Thread
+        </Button>
+      </form>
+    </Form>
+  );
+}
+
+export default PostThread;
